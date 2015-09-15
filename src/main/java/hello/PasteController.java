1cf56737ac15432aac9c921ab36dff226fe7e010
neeasade <nathanisom27@gmail.com>
2015-09-15 08:25:00 -0500

    Add search.

--- src/main/java/hello/PasteController.java
+++ src/main/java/hello/PasteController.java
@@ -12,7 +12,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * Return JSON containing information about the current machine that java can gather.
  */
 @RestController
-public class PasteController{
+public class PasteController {
 
     private final AtomicLong counter = new AtomicLong();
     //Manage a list of pastes:
@@ -30,22 +30,33 @@ public class PasteController{
         mPastes.add(new Paste(counter.getAndIncrement(), aTitle, aContent));
     }
 
-    @RequestMapping("/paste")
-    public Object getPaste(@RequestParam(value="id",defaultValue="0") String aId) {
-
-
+    //consider changing to type list<paste> and returning a length one list when there is only one on return.
+    @RequestMapping("/paste/{id}")
+    public Object getPaste(@PathVariable(value="id") String aId,
+                           @RequestParam(value = "query", defaultValue = "") String aSearchQuery) {
         switch(aId)
         {
             case "all":
                 return mPastes;
             case "search":
                 // search and return all pastes with text matching query(assume + for space)
-                for ( Paste lPaste : mPastes )
-                {
-
+                if(aSearchQuery != "") {
+                    List<Paste> lReturn = new ArrayList<Paste>();
+                    for(Paste lPaste : mPastes) {
+                        // Search both content and title
+                        if ( (lPaste.getContent() + lPaste.getTitle()).contains(aSearchQuery)) {
+                            lReturn.add(lPaste);
+                        }
+                    }
+                    return lReturn;
+                }
+                else {
+                    //Fall through to not found result.
+                    aId = "-1";
                 }
 
             default:
+                // validate id
                 int lIndex = Integer.parseInt(aId);
                 if(lIndex < 0 || lIndex > mPastes.size()-1)
                 {
