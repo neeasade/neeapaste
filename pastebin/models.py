355f48fb246fa29f483d5dd114a56807d5634196
neeasade <nathanisom27@gmail.com>
2015-09-24 11:55:00 -0500

    correct models.

--- pastebin/models.py
+++ pastebin/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Paste(model.Model):
+class Paste(models.Model):
     content = models.CharField(max_length=65535)
     title = models.CharField(max_length=30)
     views = models.IntegerField(default=0)
 
-class User(model.Model):
+class User(models.Model):
     username = models.CharField(max_length=40)
     password = models.CharField(max_length=40)
 
