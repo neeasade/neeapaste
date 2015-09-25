c622a905fcaaa9ecf590e6603dd6860772f63210
neeasade <nathanisom27@gmail.com>
2015-09-25 15:01:29 -0500

    Start of routing.

--- python-django/pastebin/views.py
+++ python-django/pastebin/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from pastebin.models import Paste, User
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Index view placeholder.")
+
+def paste(request):
+     if request.method == 'GET':
+        # Get paste model and return tostring
+        return Paste.objects.all()
